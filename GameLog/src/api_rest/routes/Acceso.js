2b822b3e8cbc25c70e7143ee16101af26c23f271
chris <chrisvasquez985@gmail.com>
2025-04-24 22:50:55 -0600

    Arreglo de validaciones

--- GameLog/src/api_rest/routes/Acceso.js
+++ GameLog/src/api_rest/routes/Acceso.js
@@ -7,7 +7,8 @@ export const CrearRutaAcceso = ({ModeloAcceso}) =>
     const AccesoEnrutador = Router();
     const ControladorAccesoEnrutador = new AccesoControlador({ModeloAcceso});
     AccesoEnrutador.post('/',ControladorAccesoEnrutador.RegistrarAcceso);
-    AccesoEnrutador.patch('/');
+    AccesoEnrutador.patch('/:id');
+    AccesoEnrutador.patch('/:id/estado');
 
     return AccesoEnrutador;
 }
