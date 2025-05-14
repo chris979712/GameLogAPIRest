09409df3b062ddb26502ded055197a23f9da6b29
chris <chrisvasquez985@gmail.com>
2025-05-13 21:58:25 -0600

    Implementacion APIgRPC para fotos de perfil de usuarios

---
+++ GameLog/APIgRPC/API/controllers/FotosDePerfilControlador.py
@@ -0,0 +1,62 @@
+import grpc
+import os
+from ..ficheros.fotosDePerfil import Fotos_De_Perfil_pb2,Fotos_De_Perfil_pb2_grpc
+
+class FotosDePerfilControlador(Fotos_De_Perfil_pb2_grpc.FotosDePerfilServicer):
+    def SubirFotoDeUsuario(self, request, context):
+        nombreDeUsuario = request.nombreDeUsuario
+        datosImagen = request.datos
+        rutaBase = os.path.abspath(os.getcwd())
+        carpetaDeFotos = os.path.join(rutaBase,os.getenv("CARPETAFOTOS"))
+        rutaFinalArchivo = os.path.join(carpetaDeFotos,f"{nombreDeUsuario}.jpg")
+
+        try:
+            with open(rutaFinalArchivo,"wb") as archivo:
+                archivo.write(datosImagen)
+        except Exception:
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(f"Error al querer guardar la imagen en el servidor")
+            return Fotos_De_Perfil_pb2.FotoDePerfilRuta(rutaArchivo="Error")
+        
+        context.set_code(grpc.StatusCode.OK)
+        context.set_details(f"Foto guardada de manera correcta")
+        return Fotos_De_Perfil_pb2.FotoDePerfilRuta(rutaArchivo=rutaFinalArchivo)
+    
+    def ObtenerFotoDePerfilUsuario(self, request, context):
+        rutaImagen = request.rutaArchivo
+        nombreDeUsuarioFoto = os.path.splitext(os.path.basename(rutaImagen))[0]
+        try:
+            with open(rutaImagen,"rb") as archivo:
+                datos = archivo.read()
+        except FileNotFoundError:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details("La imagen de perfil no fue encontrada.")
+            return Fotos_De_Perfil_pb2.FotoDePerfilDatos(nombreDeUsuario="Imagen no encontrada", datos=b"")
+        except Exception as e:
+                context.set_code(grpc.StatusCode.INTERNAL)
+                context.set_details(f"Error al leer la imagen")
+                return Fotos_De_Perfil_pb2.FotoDePerfilDatos(nombreDeUsuario="Error al leer la imagen", datos=b"")
+        return Fotos_De_Perfil_pb2.FotoDePerfilDatos(nombreDeUsuario=nombreDeUsuarioFoto,datos=datos)
+    
+    def ActualizarFotoDePerfil(self, request, context):
+        nombreDeUsuario = request.nombreDeUsuario
+        rutaImagenAntigua = request.rutaImagenAntigua
+        datosNuevaImagen = request.datos
+        rutaBase = os.path.abspath(os.getcwd())
+        carpetaDeFotos = os.path.join(rutaBase,os.getenv("CARPETAFOTOS"))
+        rutaFinalArchivo = os.path.join(carpetaDeFotos,f"{nombreDeUsuario}.jpg")
+
+        try:
+            if os.path.exists(rutaImagenAntigua) or os.path.abspath(rutaImagenAntigua) != os.path.abspath(rutaFinalArchivo):
+                os.remove(rutaImagenAntigua)
+            with open(rutaFinalArchivo,"wb") as archivo:
+                archivo.write(datosNuevaImagen)
+        except Exception as e:
+            print(e)
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(f"Error al querer actualizar la imagen en el servidor")
+            return Fotos_De_Perfil_pb2.FotoDePerfilRuta(rutaArchivo="Error")
+        
+        context.set_code(grpc.StatusCode.OK)
+        context.set_details(f"Foto actualizada de manera correcta")
+        return Fotos_De_Perfil_pb2.FotoDePerfilRuta(rutaArchivo=rutaFinalArchivo)
---
+++ GameLog/APIgRPC/server.py
@@ -0,0 +1,30 @@
+import grpc
+import os
+from dotenv import load_dotenv
+from concurrent import futures
+from API.controllers.FotosDePerfilControlador import FotosDePerfilControlador 
+from API.ficheros.fotosDePerfil import Fotos_De_Perfil_pb2_grpc
+
+load_dotenv()
+
+if not os.path.exists(os.getenv("DIRECTORIO_FOTOS")):
+    os.makedirs(os.getenv("DIRECTORIO_FOTOS"))
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10),
+             options=[("grpc.max_send_message_length", 5 * 1024 * 1024), 
+             ("grpc.max_receive_message_length", 5 * 1024 * 1024)])
+    Fotos_De_Perfil_pb2_grpc.add_FotosDePerfilServicer_to_server(FotosDePerfilControlador(),server)
+    server.add_insecure_port(f'[::]:{os.getenv("PUERTO_SERVIDOR")}')
+    server.start()
+    print(f'Servidor corriendo en el puerto: {os.getenv("PUERTO_SERVIDOR")}')
+    try:
+        server.wait_for_termination()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    serve()
